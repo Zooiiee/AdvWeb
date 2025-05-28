00cf9e92b80434c299d6a3acbccb7543136a5997
ZoyaJ1 <zoyajetham10@gmail.com>
2025-05-28 08:16:58 +0530

    Initial commit

---
+++ MVC_App/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using MVC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ReturnGift()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View(); // Looks for Views/Home/About.cshtml
+        }
+
+        public ActionResult Contact()
+        {
+            return View(); // Looks for Views/Home/Contact.cshtml
+        }
+
+        public ActionResult RSVP()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RSVP(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save to DB or process response here
+                return RedirectToAction("Thanks");
+            }
+
+            return View(response);
+        }
+
+    }
+}
---
+++ MVC_App/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App.Models
+{
+    public class GuestResponse
+    {
+        public virtual int GuestResponseId { get; set; }
+        public virtual String GuestResponseName { get; set; }
+        public virtual int GuestResponseAge { get; set; }
+        public virtual int GuestResponseMoNo { get; set; }
+        public virtual int GuestResponseVeg { get; set; }
+        public virtual int GuestResponseRSVP { get; set; }
+        public virtual String GuestResponseEmail { get; set; }
+    }
+}
